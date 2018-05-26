00cc27360f200f12f87befb6b790c9b158bc560a
khalil0M <khalil.kouiss@gmail.com>
2018-05-26 17:12:57 +0200

    First commit

--- core/src/main/java/com/models/user/userModel.java
+++ core/src/main/java/com/models/user/userModel.java
@@ -4,13 +4,20 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.Model;
 
 import javax.inject.Inject;
+i
 
 @Model(adaptables=SlingHttpServletRequest.class)
 public class userModel {
 
+    @Inject
     private String email;
-
+    @Inject
     private String pwd;
+
+    @Inject
+    public userModel(String email) {
+        this.email = email;
+    }
 
     public String getEmail() { return email; }
     public String getPwd() { return pwd; }
